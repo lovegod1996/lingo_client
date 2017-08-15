a9c41e1d540a1b5ca366b79685e858f40fd29ade
Lancer <416086226@qq.com>
2017-08-15 11:11:08 +0800

    修改足迹

--- app/src/main/java/com/lovegod/newbuy/view/myinfo/track/Track_Activity.java
+++ app/src/main/java/com/lovegod/newbuy/view/myinfo/track/Track_Activity.java
@@ -5,25 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.lovegod.newbuy.R;
 import com.lovegod.newbuy.api.BaseObserver;
 import com.lovegod.newbuy.api.NetWorks;
+import com.lovegod.newbuy.bean.Order;
 import com.lovegod.newbuy.bean.Track;
 import com.lovegod.newbuy.bean.User;
 import com.lovegod.newbuy.utils.system.SpUtils;
 import com.lovegod.newbuy.utils.view.AdapterWrapper;
 import com.lovegod.newbuy.view.TimeStickyDecoration;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 public class Track_Activity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private List<Track>trackList=new ArrayList<>();
     private List<String>timeList=new ArrayList<>();
+    private Toolbar toolbar;
     private TrackAdapter adapter;
     private AdapterWrapper wrapper;
     private int currentPage;
@@ -34,6 +43,8 @@ public class Track_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_track);
         user=(User) SpUtils.getObject(this,"userinfo");
         recyclerView=(RecyclerView)findViewById(R.id.track_recyclerview);
+        toolbar=(Toolbar)findViewById(R.id.track_toolbar);
+        setSupportActionBar(toolbar);
 
         //初始化布局管理器以及适配器
         LinearLayoutManager manager=new LinearLayoutManager(this);
@@ -44,10 +55,16 @@ public class Track_Activity extends AppCompatActivity {
         wrapper=new AdapterWrapper(this,adapter,emptyLayout);
         recyclerView.setAdapter(wrapper);
         recyclerView.addItemDecoration(new TimeStickyDecoration(this,timeList,wrapper));
-        currentPage=0;
 
-        //获取足迹数据
-        getTrack();
+        /**
+         * 返回监听
+         */
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     /**
@@ -70,5 +87,17 @@ public class Track_Activity extends AppCompatActivity {
 
             }
         });
+    }
+
+    /**
+     * 当活动为可运行状态，获取最新足迹信息
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        currentPage=0;
+        trackList.clear();
+        timeList.clear();
+        getTrack();
     }
 }
